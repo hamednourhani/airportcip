d114f7259d28f15a54f3cadc3efd165d60bb1292
Hamed Nourhani <info@itstar.ir>
2016-04-09 09:04:28 +0430

    some changes

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -91,4 +91,26 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSay($message = 'Hello')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
+
+    public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // valid data received in $model
+
+            // do something meaningful here about $model ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // either the page is initially displayed or there is some validation error
+            return $this->render('entry', ['model' => $model]);
+        }
+    }
+
 }
